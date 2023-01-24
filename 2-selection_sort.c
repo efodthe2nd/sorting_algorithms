509932b77c1a0efae8b1324a8bc720e042ac1183
efodthe2nd <davidefod@gmail.com>
2023-01-24 17:24:04 +0000

    fix 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -32,13 +32,13 @@ void selection_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min = array i;
-		for (j = i 1; j < size; j++)
-			min = (array[j] < *min) ? (array j) : min;
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
 
-		if ((array i) != min)
+		if ((array + i) != min)
 		{
-			swap_ints(array i, min);
+			swap_ints(array + i, min);
 			print_array(array, size);
 		}
 	}
